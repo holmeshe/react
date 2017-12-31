087f6278fd6df15d0fdfabd601a1e5cf6ab63a44
holmeshe <holmes@Holmess-Mac-Pro.local>
2017-12-31 14:51:36 +1300

    replace example with that used in articles

---
+++ examples/index.simple.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <h1 style={{"color":"blue"}}>hello world</h1>,
+  document.getElementById('root')
+);
+
